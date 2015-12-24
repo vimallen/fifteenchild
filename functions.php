2405e9bd78d2414f90d018bc1488905dd08e5c66
Dave Allen <davidallen401@btinternet.com>
2015-12-24 22:09:02 +0000

    Media lib issue

--- functions.php
+++ functions.php
@@ -1,7 +1,28 @@
 <?php
-add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-function theme_enqueue_styles() {
-    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+       add_action( 'wp_enqueue_scripts', 'enqueue_parent_theme_style' );
+       function enqueue_parent_theme_style() {
+             wp_enqueue_style( 'parent-style',    get_template_directory_uri().'/style.css' );
+       }
 
-}
-?>
+// Creates contact post type
+  register_post_type('panels-home', array(
+  'label' => 'Panels-home',
+  'public' => true,
+  'show_ui' => true,
+  'capability_type' => 'post',
+  'hierarchical' => false,
+  'rewrite' => array('slug' => 'panels-home'),
+  'query_var' => true,
+  'supports' => array(
+  'title',
+  'editor',
+  'excerpt',
+  'trackbacks',
+  'custom-fields',
+  'comments',
+  'revisions',
+  'thumbnail',
+  'author',
+  'page-attributes',)
+  ) );
+?><!--closing-->
---
+++ page-panels-home.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * * Template Name: page-panels-home
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area">
+	<main id="main" class="site-main" role="main">
+		<div class="grid grid-pad main" >
+		<h2><?php the_title(); ?></h2>
+		<?php
+		          $loop = new WP_Query(array('post_type' => 'panels-home',
+		          'posts_per_page' => 10,
+		          'orderby'=>'title',
+		           'order'=>'ASC',
+		          ));
+		     ?>
+		     <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
+		     <?php
+		          $custom = get_post_custom($post->ID);
+		          $screenshot_url = $custom["screenshot_url"][0];
+		          $website_url = $custom["website_url"][0];
+		     ?>
+
+		<div class="col-1-3">
+			<div class="module-contact">
+				<h4 class="underline-dashed"><?php the_title(); ?></h4>
+			<?php the_post_thumbnail(); ?> </a>
+		<?php the_content(); ?>
+		</div><!-- module -->
+		</div><!--col-->
+
+
+		<?php endwhile; ?>
+		</div><!--grid-->
+
+	</main><!-- .site-main -->
+
+
+
+</div><!-- .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
