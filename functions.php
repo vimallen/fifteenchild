6c1578b4f05d27529ec4f5dd23aff84df3787b82
Dave Allen <davidallen401@btinternet.com>
2016-01-05 20:43:46 +0000

    Added collection cpt

--- functions.php
+++ functions.php
@@ -24,4 +24,25 @@
   'author',
   'page-attributes',)
   ) );
-?>
+
+  register_post_type('module-collection', array(
+    'label' => 'Module-Collection',
+    'public' => true,
+    'show_ui' => true,
+    'capability_type' => 'post',
+    'hierarchical' => false,
+    'rewrite' => array('slug' => 'module-collection'),
+    'query_var' => true,
+    'supports' => array(
+    'title',
+    'editor',
+    'excerpt',
+    'trackbacks',
+    'custom-fields',
+    'comments',
+    'revisions',
+    'thumbnail',
+    'author',
+    'page-attributes',)
+    ) );
+  ?>
